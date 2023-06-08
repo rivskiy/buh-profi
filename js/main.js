3e3e153607b9fbf049ff37979092b7e4d1e21290
Aleksey Pechurin <pe4urin@yandex.ru>
2023-06-08 13:35:16 +0300

    добавлены и стили

---
+++ js/main.js
@@ -0,0 +1,47 @@
+                  // MENU
+const menu = document.querySelector('.header__list');
+const menuBtn = document.querySelector('.menu-btn');
+const menuLinks = document.querySelectorAll('.nav__link');
+
+const showMenu = () => {
+  menu.classList.toggle('header__list--active');
+  menuBtn.classList.toggle('menu-btn--active');
+};
+
+menuBtn.addEventListener('click', showMenu);
+
+for (let link of menuLinks) {
+  link.addEventListener('click', showMenu)
+};
+
+
+                  // SLIDER
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 500,
+  freeMode: true,
+  followFinger: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
